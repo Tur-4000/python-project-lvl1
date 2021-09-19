cdfae4763095497253cb6418b98795898d6b00ec
Turbanov Valeriy <tur.4000@gmail.com>
2021-09-19 23:36:24 +0300

    Prime Game

---
+++ brain_games/games/prime_game.py
@@ -0,0 +1,47 @@
+"""Even Game."""
+from random import randint
+
+import prompt
+from brain_games.game import run_game
+
+
+def prime_game():
+    """Play Prime Game."""
+    instruction = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+
+    run_game(get_game_data, instruction)
+
+
+def get_game_data():
+    """
+    Prepare game data.
+
+    Returns:
+        tuple
+    """
+    question = randint(1, 100)
+    right_answer = 'yes' if is_prime(question) else 'no'
+    print('Question: {0}'.format(question))
+    answer = prompt.string('Your answer: ')
+
+    return (answer, right_answer)
+
+
+def is_prime(number):
+    """
+    Check is prime number.
+
+    Args:
+        number: checked number
+
+    Returns:
+        bool
+    """
+    if number < 2:
+        return False
+    div = 2
+    
+    while number % div != 0:
+        div += 1
+
+    return div == number
---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""'Brain Prime' Play."""
+
+from brain_games.games.prime_game import prime_game
+
+
+def main():
+    """Main function of Brain Prime."""
+    prime_game()
+
+
+if __name__ == '__main__':
+    main()
